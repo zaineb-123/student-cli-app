3d7a76cc03115df6bd6e3951032642605c4be3f1
Houcem Souid <houcem.sd@gmail.com>
2025-06-23 10:54:24 +0200

    Add comments to improve naming and validations

--- application1.js
+++ application1.js
@@ -7,26 +7,32 @@ import readLineSync, { question } from 'readline-sync';
 
 
 const sleep = (ms = 2000) => new Promise(resolve => setTimeout(resolve, ms));
+//==== Use meaningful english naming for functions for example:
+// bienvenu() => showWelcomeText()
 async function bienvenu() {
     const title = chalkAnimation.rainbow("Espace Etudiant\n");
     await sleep(2000);
     title.stop();
-
+//==== Remove unnacessary extra lines
     
 }
 
+//==== Use meaningful english naming for functions for example:
+// valideremail() => validateEmail()
 function valideremail(email){
+    //==== Use a regex to validate an email, not all user have gmail 
     const estvalide=/^[a-zA-Z0-9]+@gmail\.com$/;
     return estvalide.test(email);
 }
 
-
+//==== Use meaningful english naming for functions
 function valideranne(annee){
+    //==== Add more validation for the year, the year should be between 2000 and 2025
     const estvalide=/^\d{4}$/;
     return estvalide.test(annee);
 }
 
-
+//==== Use meaningful english naming for functions
 async function ajouteretudiant() {
     console.log(chalk.blue.bold.italic('veuillez saisir vos informations personelles'));
     const question = await inquirer.prompt([
@@ -47,7 +53,7 @@ async function ajouteretudiant() {
     return resultat;
 }
 
-
+//==== Use meaningful english naming for functions
 async function question1() {
     const {filiere} = await inquirer.prompt({
         name:'filiere',
@@ -60,7 +66,7 @@ async function question1() {
     
 }
 
-
+//==== Use meaningful english naming for functions
 async function question2() {
     const {classe} = await inquirer.prompt({
         name:'classe',
